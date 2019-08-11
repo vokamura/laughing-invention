febe2740f145eba3ecc82ecd2ad6cb6c2263e3ec
Vikki Okamura <vcokamura@gmail.com>
2019-08-11 16:33:45 -0700

    Practice 08-11-19

---
+++ adding.js
@@ -0,0 +1,10 @@
+function SimpleAdding(num) { 
+    let num2 = 0;
+    // code goes here  
+    for(let i = 0; i <= num; i++){
+        num2 = num2 + i;
+    }
+    return num2; 
+           
+  }
+  SimpleAdding(4);
---
+++ factorial.js
@@ -0,0 +1,12 @@
+function FirstFactorial(num) { 
+
+    // code goes here  
+    let num2 = 1;
+    for(let i = 1; i <= num; i++){
+        num2 = num2 * i
+    }
+    return num; 
+           
+  }
+  
+  FirstFactorial(4);
